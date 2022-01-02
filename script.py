76c4d705dde3964b75f829fd857db2d109c5b559
codecademy <ccuser@codecademy.com>
2022-01-02 18:44:37 +0000

    Added interest finder logic

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
-destinations = ["Paris, France", "Shangai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
-test_traveler = ['Erin Wilkes', 'Shangai, China', ['historical site', 'art']]
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
 def get_destination_index(destination):
   destination_index = destinations.index(destination)
@@ -36,3 +36,19 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+def find_attractions(destination, interests):
+  destination_index = get_destination_index(destination)
+  attractions_in_city = attractions[destination_index]
+  attractions_with_interest = []
+  for attraction in attractions_in_city:
+    possible_attraction = attraction
+    attraction_tags = attraction[1]
+
+    for interest in interests:
+      if interest in attraction_tags:
+        attractions_with_interest.append(possible_attraction[0])
+  return attractions_with_interest
+
+la_arts = find_attractions("Los Angeles, USA", ['art'])
+#print(la_arts)
